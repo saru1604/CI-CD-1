760920d518d5e23392b50becfe2d001ea41d6bdb
Hiren Parghi <hirenparghi90@gmail.com>
2022-05-18 19:57:04 +0530

    7:57

--- source/models/model.js
+++
@@ -1,5 +0,0 @@
-// *************************************************************
-// set up POST API into a Database - MongoDB, etc.
-// *************************************************************
-
-// this demo doesn't connect to a DB at the moment
